72e54afa393983157abdbee0c4ad74459b3ef492
Samuel Njenga <snjenga704@gmail.com>
2023-04-19 15:09:37 +0300

    feat: add pre version of GetDeepValue util

---
+++ src/components/values/GetDeepValue.ts
@@ -0,0 +1,18 @@
+export const getDeepValue = (obj: any, firstKey: string, secondKey: string) => {
+  return obj[firstKey][secondKey];
+};
+
+const obj = {
+  foo: {
+    a: true,
+    b: 2,
+  },
+  bar: {
+    c: "12",
+    d: 18,
+  },
+};
+
+const value = getDeepValue(obj, "foo", "a");
+
+typeof value === "number";
