7ee05a97f69d2e95c7e86dd5cccbaf1472d88df2
Samuel Njenga <snjenga704@gmail.com>
2023-04-19 15:27:39 +0300

    feat: add post version of GetDeepValue util

---
+++ src/components/values/GetDeepValueV2.ts
@@ -0,0 +1,27 @@
+// One Generic relying upon one another
+export const getDeepValue = <
+  TObj,
+  TFirstKey extends keyof TObj,
+  TSecondKey extends keyof TObj[TFirstKey]
+>(
+  obj: TObj,
+  firstKey: TFirstKey,
+  secondKey: TSecondKey
+) => {
+  return obj[firstKey][secondKey];
+};
+
+const obj = {
+  foo: {
+    a: true,
+    b: 2,
+  },
+  bar: {
+    c: "12",
+    d: 18,
+  },
+};
+
+const value = getDeepValue(obj, "foo", "a");
+
+typeof value === "number";
